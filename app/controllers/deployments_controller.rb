5872b313e4407bf224f892dec7a04c32521ff744
Isel Fernandez <isel_77@hotmail.com>
2012-05-15 16:53:00 -0400

    passing the author in the query string

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -2,6 +2,6 @@ require_relative '../../app/models/deployment_repo'
 
 class DeploymentsController < ApplicationController
   def list
-    @deployments = DeploymentRepo.new.all
+    @deployments = DeploymentRepo.new.all(params[:author])
   end
 end
--- spec/controllers/deployments_controller_spec.rb
+++ spec/controllers/deployments_controller_spec.rb
@@ -6,6 +6,8 @@ describe DeploymentsController do #, :type => :controller do
     it "returns http success" do
       get 'list'
       response.should be_success
+      response.should have_selector("td",
+        :content => "#{@base_title} | Home")
     end
   end
 
