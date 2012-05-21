a1008d618ff784aadccb03d4a979f82215d80ee8
Isel Fernandez <isel_77@hotmail.com>
2012-05-21 11:08:29 -0400

    adding base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Api::Application.config.secret_token = 'e78d33c6c39e914f182d80bc0bd8f14516784414d220dec60a42fa13ce97901ff4bf0e17c1f0f620c8581617a4bf45681d3af4577f088b9522ec7b7a86f9f1b7'
